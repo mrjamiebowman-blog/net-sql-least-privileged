06fce18009ace8dd76991b4de5ce6620fbe51193
Jamie Bowman <mrjamiebowman@gmail.com>
2021-03-24 20:02:10 -0400

    initial repo code for dapper

---
+++ src/WebAppSP/Repositories/IStudentRepository.cs
@@ -0,0 +1,10 @@
+﻿using WebAppSP.Models;
+
+namespace WebAppSP.Repositories
+{
+    public interface IStudentRepository
+    {
+        Student GetStudentByIdAsync(int tenantId, int studentId);
+        Student UpdateStudentsGradeAsync(int tenantId, int studentId, string grade);
+    }
+}
---
+++ src/WebAppSP/Repositories/SqlStudentRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppSP.Models;
+
+namespace WebAppSP.Repositories
+{
+    public class SqlStudentRepository : IStudentRepository
+    {
+        public Student GetStudentByIdAsync(int tenantId, int studentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student UpdateStudentsGradeAsync(int tenantId, int studentId, string grade)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
